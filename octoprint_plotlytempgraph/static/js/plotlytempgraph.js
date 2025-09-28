67dfd333fb5a5b38b4e47ace4bdc70a393abf6d0
Andrew Erickson <aerickson@gmail.com>
2025-09-27 19:04:23 -0700

    add some dummy data

--- octoprint_plotlytempgraph/static/js/plotlytempgraph.js
+++ octoprint_plotlytempgraph/static/js/plotlytempgraph.js
@@ -106,6 +106,40 @@ $(function() {
 
 		Plotly.newPlot('plotlytempgraph', self.data, self.layout, self.options);
 
+        // Dummy data for each axis
+        const now = new Date();
+        const dummyX = [now, new Date(now.getTime() + 60000), new Date(now.getTime() + 120000)];
+
+        const dummyTraces = [
+            {
+                name: 'Temperature',
+                x: dummyX,
+                y: [20, 25, 30],
+                yaxis: 'y',
+                mode: 'lines',
+                line: {color: '#1f77b4'}
+            },
+            {
+                name: 'AQI',
+                x: dummyX,
+                y: [1, 3, 5],
+                yaxis: 'y2',
+                mode: 'lines',
+                line: {color: '#ff7f0e'}
+            },
+            {
+                name: 'TVOC/eCO2',
+                x: dummyX,
+                y: [400, 600, 800],
+                yaxis: 'y3',
+                mode: 'lines',
+                line: {color: '#2ca02c'}
+            }
+        ];
+
+        // Add the dummy traces to the graph
+        Plotly.addTraces('plotlytempgraph', dummyTraces);
+
         self.settingsViewModel.addNameMapping = function() {
             self.settingsViewModel.settings.plugins.plotlytempgraph.name_map.push({"identifier": ko.observable(""),
                 "label": ko.observable(""),
